dcd9170e678157a3e02249e206aab30f69578707
Joel Ibaceta <joelibaceta@MacBook-Pro-de-Joel.local>
2018-07-26 04:40:46 -0300

    rollback

--- app/bot/listen.rb
+++ app/bot/listen.rb
@@ -4,5 +4,5 @@ include Facebook::Messenger
 Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["ACCESS_TOKEN"])
 
 Bot.on :message do |message|
-    message.reply({text: 'Your current snippet is:' + session[:current_snippet].to_s})
+    message.reply(text: message.inspect)
 end
