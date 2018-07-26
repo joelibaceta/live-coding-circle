c84b934f5221f013b37d7a02362b2b37c1a6d140
Joel Ibaceta <joelibaceta@MacBook-Pro-de-Joel.local>
2018-07-26 06:44:52 -0300

    fixing

--- db/migrate/20180726065616_create_messages.rb
+++ db/migrate/20180726065616_create_messages.rb
@@ -3,8 +3,7 @@ class CreateMessages < ActiveRecord::Migration[5.2]
     create_table :messages do |t|
       t.string :snippet_id
       t.string :author
-      t.string :body
-
+      t.string :body 
       t.timestamps
     end
   end
--- db/migrate/20180726071805_add_user_id_to_message.rb
+++
@@ -1,5 +0,0 @@
-class AddUserIdToMessages < ActiveRecord::Migration[5.2]
-  def change
-    add_column :messages, :user_id, :string
-  end
-end
