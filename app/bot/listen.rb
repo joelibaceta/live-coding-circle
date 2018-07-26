cef41bc2f7c3346af8ecc80eb747f91526f3c2bf
Joel Ibaceta <joelibaceta@MacBook-Pro-de-Joel.local>
2018-07-26 13:54:05 -0300

    broadcast

--- app/bot/listen.rb
+++ app/bot/listen.rb
@@ -18,6 +18,7 @@ def GetAccessToken
 end
 
 def BuildMessage(msg)
+    
     uri = URI("https://graph.facebook.com/v3.0/me/message_creatives?access_token=#EAAGJcZArwqG0BAAm1rK3iW2fBFMQGsiJ4No061CZAr21wngsiNDKzbcgYgoNkhop2pGZBriQjoQtKLVfLnNVx2l3uoZCBoTnO7QWHjJM3ZBubFYZClyE4PWoZCgogZBolD5TjrJwMJ5DGnMCtPB4vNViziUSmIzjg8VNXlmau0AVuwZDZD")
 
     request = Net::HTTP::Post.new(uri)
@@ -44,6 +45,7 @@ def BuildMessage(msg)
 end
 
 def sendBroadcast(msg_id)
+    uri = URI.parse("https://graph.facebook.com/v3.0/me/broadcast_messages?access_token=EAAGJcZArwqG0BAAm1rK3iW2fBFMQGsiJ4No061CZAr21wngsiNDKzbcgYgoNkhop2pGZBriQjoQtKLVfLnNVx2l3uoZCBoTnO7QWHjJM3ZBubFYZClyE4PWoZCgogZBolD5TjrJwMJ5DGnMCtPB4vNViziUSmIzjg8VNXlmau0AVuwZDZD")
     request = Net::HTTP::Post.new(uri)
     request.content_type = "application/json"
     request["Cache-Control"] = "no-cache"
