f1d25c0adfbb481e089cfedddbeaa467f4a7054d
Joel Ibaceta <joelibaceta@MacBook-Pro-de-Joel.local>
2018-07-26 00:43:34 -0300

    include token

--- app/bot/listen.rb
+++ app/bot/listen.rb
@@ -1,7 +1,7 @@
 require "facebook/messenger"
 include Facebook::Messenger
 
-Facebook::Messenger::Subscriptions.subscribe(access_token: "")
+Facebook::Messenger::Subscriptions.subscribe(access_token: "EAAGJcZArwqG0BAE8vwG8C9PSsNerkqCqvj8RJWgYWQJRO8ueftYVPRwl2pwTx383cZCMzfEwx3MNHZAn6UDMSvhg584kxoqV2HRKnAs3H1uloOZAWTJjnOZBpbQMLlZB6FlM2RRUHq6vgDK42seCiNjyfZAVhFBpFpuHnzo5xDiNAZDZD")
 
 Bot.on :message do |message|
   puts "got your message!"
