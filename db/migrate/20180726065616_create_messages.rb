7086ddc58cdd3c39a4435acd37343b1401c54e3c
Joel Ibaceta <joelibaceta@MacBook-Pro-de-Joel.local>
2018-07-26 06:40:11 -0300

    typo

--- db/migrate/20180726065616_create_messages.rb
+++ db/migrate/20180726065616_create_messages.rb
@@ -1,4 +1,4 @@
-class CreateMessage < ActiveRecord::Migration[5.2]
+class CreateMessages < ActiveRecord::Migration[5.2]
   def change
     create_table :messages do |t|
       t.string :snippet_id
--- db/migrate/20180726071805_add_user_id_to_message.rb
+++ db/migrate/20180726071805_add_user_id_to_message.rb
@@ -1,4 +1,4 @@
-class AddUserIdToMessage < ActiveRecord::Migration[5.2]
+class AddUserIdToMessages < ActiveRecord::Migration[5.2]
   def change
     add_column :messages, :user_id, :string
   end
