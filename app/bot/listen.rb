db50a839577347716138c058d44c63c7c0b28e74
Joel Ibaceta <joelibaceta@MacBook-Pro-de-Joel.local>
2018-07-26 05:46:06 -0300

    inspection for debug

--- app/bot/listen.rb
+++ app/bot/listen.rb
@@ -14,8 +14,8 @@ Bot.on :message do |message|
     filtered_messages = Message.where("author != ?", sender).map{|msg| "#{msg.author}: #{msg.message}"}
     filtered_reponses = Reponse.where("sender_id != ?", sender).map{|msg| "#{msg.sender_id}: #{msg.message}"}
 
-    puts "FILTERED MESSAGES: " + filtered_messages
-    puts "FILTERED REPONSES: " + filtered_reponses
+    puts "FILTERED MESSAGES: " + filtered_messages.inspect
+    puts "FILTERED REPONSES: " + filtered_reponses.inspect
 
     messages_to_send = filtered_messages - filtered_reponses
 
