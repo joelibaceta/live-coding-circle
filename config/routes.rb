ffee1f62780069829e7a70d1b624108716e715e1
joel ibaceta <mail@joelibaceta.com>
2018-08-20 23:09:44 -0300

    skin iteration

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do   
-  resources :snippets
+
+  root :to => 'snippets#stream'
 
   get 'snippet/:slug', to: 'snippets#show'
-  get 'snippet/:slug/stream', to: 'snippets#stream'
+  get 'snippet/:slug/stream', to: 'snippets#stream', :as => :stream
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   mount ActionCable.server => '/cable'
