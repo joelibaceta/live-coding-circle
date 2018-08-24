bb8ece2e358b5b9b8d77bfb02a375590d9522f2e
joelibaceta <joel.ibaceta@mercadolibre.com>
2018-08-24 10:43:19 -0300

    terminal works

--- app/channels/terminal_channel.rb
+++ app/channels/terminal_channel.rb
@@ -3,63 +3,49 @@ require_relative '../lib/session.rb'
 class TerminalChannel < ApplicationCable::Channel
 
     @session = nil
-    @channel = nil
 
     attr_accessor :channel
 
     def subscribed
+
+        @semaphore = Mutex.new
+
+        term_session_name = "terminal_#{params[:session_id]}"
  
         stream_from "terminal_#{params[:session_id]}"
-
-        @session = Session.new(params[:session_id])
-
-        @session.ssh_connection = Session::SSHConnection.new(
-            host: "206.81.9.224",
-            user: "developer",
-            password: "r3dc0d3"
-        )
-
-        @session.start()
-
-        @channel = @session.current_session.open_channel do  |ch|
-
-            puts ch 
-
-            ch.on_data do |ch, data|
-                #puts data
-                ActionCable.server.broadcast("terminal_#{params[:session_id]}", data)
-            end
-        
-            ch.on_extended_data do |ch, type, data|
-                puts "stderr"
-            end
-        
-            ch.on_close { puts "done!" }
-
-            ch.request_pty
-
-            ch.send_channel_request("shell")
-
-            puts self
-            
-        end 
  
-        @session.current_session.loop
+
+        Thread.start do
+            @semaphore.synchronize {
+            
+                ssh = Net::SSH.start('206.81.9.224', 'developer', password: "r3dc0d3")
+        
+                @channel = ssh.open_channel do |ch|
+                     
+                    ch.on_data do |ch, data|
+                        ActionCable.server.broadcast(term_session_name, data)
+                    end
+                    ch.on_extended_data do |ch, type, data|
+                        puts "stderr"
+                    end
+            
+                    ch.request_pty
+                    ch.send_channel_request("shell")  
+                    ch.on_close { puts "done!" } 
+            
+                    end  
+            
+                    ssh.loop(0.1) {  
+                        true 
+                    } 
+            } 
+        end
         
     end
 
-    def receive(data)  
-        puts @channel 
-        puts data
-        p @channel.send_data(data["char"]) if @channel
+    def receive(data)
+        @channel.send_data(data["char"]) if @channel
     end
 
-    
-
-    # def define_dynamic_method(name, &block)
-    #     (class << self; self; end).class_eval do
-    #         define_method name, &block
-    #     end
-    # end
   
 end
