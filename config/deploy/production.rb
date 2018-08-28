bf2e6df19c48e1c8ce10324fc2993ce004427c92
joelibaceta <joel.ibaceta@mercadolibre.com>
2018-08-28 01:58:09 -0300

    cap rvm set up

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -15,6 +15,9 @@ set :user,            'developer'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
 
+set :rvm_type, :user 
+set :rvm_ruby_version, '2.5.1'
+
 # Don't change these unless you know what you're doing
 set :pty,             true
 set :use_sudo,        false
