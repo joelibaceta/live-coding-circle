709164d430f24084f71b0dbabef4291a06721bb0
joelibaceta <joel.ibaceta@mercadolibre.com>
2018-08-30 15:05:57 -0300

    remove fil

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -3,10 +3,10 @@ class Snippet < ApplicationRecord
     after_save :update_related_file
 
     def update_related_file
-        Dir.mkdir("middleware/snippets/#{slug}") unless Dir.exist?("middleware/snippets/#{slug}")
-        file = File.open("middleware/snippets/#{slug}/#{title}", "w+")
-        file.write(code)
-        file.close
+        # Dir.mkdir("middleware/snippets/#{slug}") unless Dir.exist?("middleware/snippets/#{slug}")
+        # file = File.open("middleware/snippets/#{slug}/#{title}", "w+")
+        # file.write(code)
+        # file.close
     end
 
 end
