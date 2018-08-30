d6805ff9773393f7194161365071e4e8f5a86df5
joelibaceta <joel.ibaceta@mercadolibre.com>
2018-08-30 13:37:34 -0300

    fixing unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,5 +1,5 @@
 # set path to application
-app_dir = File.expand_path("../..", __FILE__)
+app_dir = File.expand_path("../../../..", __FILE__)
 
 shared_dir = "#{app_dir}/shared"
 working_directory app_dir
